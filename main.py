6ac7f773f095645fcba181623c321f389408f269
Leticia Raphaele <90052051+LeticiaRaphaele@users.noreply.github.com>
2023-07-29 13:10:12 -0300

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-# O código se encontra na pasta zipada com o nome correios.zip na rota home do GitHub.
-
-# O banco de dados usado para rodar a aplicação em desenvolvimento foi SQLite, mas posteriormente será migrada para o MySQL.
-
-# A API está implementada em flask e o frontend iniciamosa implementação em next.
-
-# Obs.: Nos esforçamos ao máximo e observem a nossa solução/implementação com carinho, por favor?! S2
